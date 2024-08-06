5aae9e05a3bdc2b1318d17af7a43eacf57f246f7
Pablo Anguiano <60408322+pabloanguiano@users.noreply.github.com>
2024-08-06 08:24:04 -0600

    Add files via upload
    Se añade nueva pagina de calificaciones con una tabla editable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",flowbite.content(),],
   theme: {
     extend: {},
+    colors: {
+      'rose':'#f43f5e'
+    },
   },
-  plugins: [],
+  plugins: [flowbite.plugin(),],
 }
+
